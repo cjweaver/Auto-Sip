93ebdf89a8c9f96906682d819fdb989dfe3c91e9
cjweaver <christopher.john.weaver@gmail.com>
2020-11-10 22:48:29 +0000

    Adjusted filename hints

--- autosip/helpers/checkfilenames.py
+++ autosip/helpers/checkfilenames.py
@@ -12,12 +12,12 @@ bl_regex = r"[A-Z]{2,5}_[A-Za-z0-9-]{1,}(_i\d{1,3})?_s([0-9]{1}|[1-9]{1,2})_f\d{
 
 # Same regex as above split into named filename fields. 
 bl_regex_segments = {
-                    'orignator': r"^[A-Z]{2,5}$", 
-                    'shelf mark': r"^[A-Za-z0-9-]{1,}$", 
-                    'item': r"^i([1-9]{1}|[0-9]{1,3})$", 
-                    'side': r"^s([0-9]{1}|[1-9]{1,2})$", 
-                    'file': r"^f\d{2}$", 
-                    'version': r"^v\d{1,2}$"
+                    'orignator': r"[A-Z]{2,5}", 
+                    'shelf mark': r"[A-Za-z0-9-]{1,}", 
+                    'item': r"i([1-9]{1}|[0-9]{1,3})", 
+                    'side': r"s([0-9]{1}|[1-9]{1,2})", 
+                    'file': r"f\d{2}", 
+                    'version': r"v\d{1,2}"
                     }
 
 # List of filename field rules. Taken from the document: "How to Create Filenames for Unlocking our Sound Heritage v0.4"
@@ -29,7 +29,7 @@ filename_hints = {
                                N.B. Items numbers are non-mandatory for a single item with a single shelf mark.\n""", 
                     'side': """Precede the side number with an ‘s’ character. For born-digital files use ‘s0’.
                                Sides are not zero padded.\n""", 
-                    'file': "This field requires two digits, so files under ten should be padded with a single zero (e.g. 01, 02, and 09)\n", 
+                    'file': "The 'file' field requires two digits, so files under ten should be padded with a single zero (e.g. 01, 02, and 09)\n", 
                     'version': """Record the version number of the file in cases where subjective choices have been made
                                   (alternate stylus choice, playback speed &c.). Do not pad with zeros\n"""
                     }
@@ -108,6 +108,7 @@ def check_reg_ex(filepaths, bl_regex, bl_regex_segments):
     errors = False
 
     # Need to differentiate between a field missing and a field being wrong.
+    # 4/11/2020 Cannot identify what field is missing.
 
     for fpath in filepaths:
         if not re.match(bl_regex, fpath.stem):
@@ -126,9 +127,9 @@ def check_reg_ex(filepaths, bl_regex, bl_regex_segments):
                     filename_errors.append(f"{error}\n{hint}")
         
         # Check for a item field accidently in the shelfmark e.g. C516-01-11-i1_s1_f01_v1
-        if list(filter(None, map(re.search, itertools.repeat('-i\d', len(fpath.stem.split("_"))), fpath.stem.split("_")))):
+        if list(filter(None, map(re.search, itertools.repeat(r'-i\d', len(fpath.stem.split("_"))), fpath.stem.split("_")))):
             errors = True
-            filename_errors.append(f"An item field appears in the shelfmark. Perhaps a hypen instead of an underscore")
+            filename_errors.append(f"An item field appears in the shelfmark in file {fpath.name}.\nPerhaps a hypen instead of an underscore")
 
     if errors:
         return (True, filename_errors)
